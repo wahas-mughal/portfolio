1173c948c6f5420a9c7a8f78ed32c618dbc6f777
Wahas Mughal <wahasali22@gmail.com>
2025-02-22 16:25:04 +0500

    Removed email

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = ({isDarkMode}) => {
     <div className='mt-20'>
       <div className='text-center'>
         <Image src={isDarkMode ? assets.logo_dark : assets.logo_white} alt='' className='w-36 mx-auto mb-2'/>
-        <div className='w-max flex items-center gap-2 mx-auto'>
+        {/* <div className='w-max flex items-center gap-2 mx-auto'>
         <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
         wahasali22@gmail.com
-        </div> 
+        </div>  */}
       </div>
       <div className='text-center sm:flex items-center justify-between border-t border-gray-400
       mx-[10%] mt-12 py-6'>
